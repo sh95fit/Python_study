b0e230584dd3d4bae209762376603532d7c27fe7
sh95fit <h95fit@gmail.com>
2022-11-14 17:25:17 +0900

    다양한 문자열 처리 함수

--- Python_Advanced/Python_venv/Advanced01.py
+++ Python_Advanced/Python_venv/Advanced01.py
@@ -8,6 +8,14 @@ python -m venv [가상환경명칭]
 
 가상환경 나가기
 deactivate
+
+bash 쉘에서 파이썬 가상환경 설정
+가상환경 생성
+python -m venv [가상환경 명칭]
+가상환경 접속
+source [가상환경 명칭]/Scripts/activate
+가상환경 나가기
+deactivate
 '''
 
 print("가상환경 구축 확인!")
