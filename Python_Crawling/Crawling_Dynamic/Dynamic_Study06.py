7ef5bf73ca41fed937b7648827d41ee00c0d5e20
sh95fit <h95fit@gmail.com>
2022-12-23 17:01:21 +0900

    동적 크롤링 프로젝트 - 중복 코드 함수화

--- Python_Crawling/Crawling_Dynamic/Dynamic_Study06.py
+++ Python_Crawling/Crawling_Dynamic/Dynamic_Study06.py
@@ -84,6 +84,27 @@ def parse_products() :
         products.append((name, price))
     return products
 
+# 제조사 선택을 위한 함수
+def choose_maker(text) :
+    # 옵션 전체 보기 선택
+    find_visible("div.search_option_title button").click()
+
+    # 제조사 선택
+    makers = finds_visible("input[name=makerCode]")
+
+    # for i in range(len(cpu_makers)) :
+    #     print(str(i+1) + ". " + cpu_makers[i].get_attribute('data'))    # get_attribute : 태그 내 속성의 값을 가져올 때 사용
+    num = choose_one(text + " 제조사", makers)
+    print(makers[num-1].get_attribute('data'))
+    # 클릭 요소가 span 태그로 되어 있어 따로 아래와 같이 span 태그+ nth-child 조합으로 활용한다
+    select = find_visible(
+        f"#estimateMainSearchOption > div > div.search_option_list > div:nth-child(1) > div.search_cate_contents > ul > li:nth-child({num}) > label > span")
+    select.click()
+
+    return num
+
+
+
 # 다나와 PC 견적 웹페이지 접속
 chrome.get(url)
 
@@ -114,10 +135,9 @@ category = {
     "파워" : "880",
 }
 
-
 category_css = {
     # Dictionary Comprehension
-    c : "dd.category_" + category[c] + " a" for c in category
+    c: "dd.category_" + category[c] + " a" for c in category
     # 반복이 필요
     # "CPU": "dd.category_" + catagory["CPU"] + " a",
     # "메인보드": "dd.category_" + catagory["메인보드"] + " a",
@@ -128,43 +148,56 @@ category_css = {
     # "파워": "dd.category_" + catagory["파워"] + " a",
 }
 
+# iframe이 있는 경우 카테고리 지정 페이지 이동
+def go_to_category(category_name) :
+    # Frame(id)
+    find_visible(category_css[category_name]).click()
+    time.sleep(1)
+
 # CPU 카테고리 클릭
-cpu = find_visible(category_css["CPU"])
-cpu.click()
+# cpu = find_visible(category_css["CPU"])
+# cpu.click()
+go_to_category("CPU")
 
 time.sleep(1)
 
 # CPU 조건 선택
 
-# 옵션 전체 보기 선택
-find_visible("div.search_option_title button").click()
+# # 옵션 전체 보기 선택
+# find_visible("div.search_option_title button").click()
 
+# # CPU 제조사 선택
+# cpu_makers = finds_visible("input[name=makerCode]")
 
-# CPU 제조사 선택
-cpu_makers = finds_visible("input[name=makerCode]")
+# # for i in range(len(cpu_makers)) :
+# #     print(str(i+1) + ". " + cpu_makers[i].get_attribute('data'))    # get_attribute : 태그 내 속성의 값을 가져올 때 사용
+# num = choose_one("CPU 제조사", cpu_makers)
+# print(cpu_makers[num-1].get_attribute('data'))
+# # 클릭 요소가 span 태그로 되어 있어 따로 아래와 같이 span 태그+ nth-child 조합으로 활용한다 
+# select = find_visible(
+#     f"#estimateMainSearchOption > div > div.search_option_list > div:nth-child(1) > div.search_cate_contents > ul > li:nth-child({num}) > label > span")
+# select.click()
 
-# for i in range(len(cpu_makers)) :
-#     print(str(i+1) + ". " + cpu_makers[i].get_attribute('data'))    # get_attribute : 태그 내 속성의 값을 가져올 때 사용
-num = choose_one("CPU 제조사", cpu_makers)
-print(cpu_makers[num-1].get_attribute('data'))
-# 클릭 요소가 span 태그로 되어 있어 따로 아래와 같이 span 태그+ nth-child 조합으로 활용한다 
-test = find_visible(
-    f"#estimateMainSearchOption > div > div.search_option_list > div:nth-child(1) > div.search_cate_contents > ul > li:nth-child({num}) > label > span")
-test.click()
+# 함수로 전환
+cpu_idx = choose_maker("CPU")
 
 
 # CPU 종류 선택
-if (num-1) == 0 :
+is_intel = False
+is_amd = False
+if (cpu_idx-1) == 0 :
+    is_intel = True
     find_visible(
-        "#estimateMainSearchOption > div > div.search_option_list > div:nth-child(2) > div.search_cate_contents > button").click()
-    options = finds_visible(
-        f"#estimateMainSearchOption > div > div.search_option_list > div:nth-child(2) > div.search_cate_contents.open > ul > li")
+        "#estimateMainSearchOption > div > div.search_option_list > div:nth-child(2) > div.search_cate_contents > button").click()  # 제조사 호환 CPU 선택
+    options = finds_visible(    
+        f"#estimateMainSearchOption > div > div.search_option_list > div:nth-child(2) > div.search_cate_contents.open > ul > li")   # 해당 CPU 리스트 불러오기
     i = choose_one_op("인텔 CPU 종류 선택", options)
     find_visible(
         f"#estimateMainSearchOption > div > div.search_option_list > div:nth-child(2) > div.search_cate_contents.open > ul > li:nth-child({i}) > label > span"
     ).click()
 
-elif (num-1) == 1 :
+elif (cpu_idx-1) == 1 :
+    is_amd = True
     find_visible(
         "#estimateMainSearchOption > div > div.search_option_list > div:nth-child(3) > div.search_cate_contents > button").click()
     options = finds_visible(
@@ -210,6 +243,71 @@ cpu_list = parse_products()
 
 for cpu in cpu_list :
     print(cpu)
+
+
+
+
+# 메인보드 선택하기
+go_to_category("메인보드")
+
+
+# 메인보드 제조사 선택
+mainboard_idx = choose_maker("메인보드")
+
+# 선택한 CPU를 통해 자동적으로 호환되는 제품 종류 선택
+if is_intel == True :
+    find_visible(
+        f"#estimateMainSearchOption > div > div.search_option_list > div:nth-child(2) > div.search_cate_contents > ul > li:nth-child(1) > label > span"
+    ).click()
+elif is_amd == True :
+    find_visible(
+        f"#estimateMainSearchOption > div > div.search_option_list > div:nth-child(2) > div.search_cate_contents > ul > li:nth-child(2) > label > span"
+    ).click()
+else :
+    find_visible(
+        f"#estimateMainSearchOption > div > div.search_option_list > div:nth-child(2) > div.search_cate_contents > ul > li:nth-child(3) > label > span"
+    ).click()
+
+time.sleep(2)
+
+mainboard_list = parse_products()
+
+for mainboard in mainboard_list:
+    print(mainboard)
+
+
+
+
+# 메모리 선택하기
+go_to_category("메모리")
+
+# 메모리 제조사 선택
+choose_maker("메모리")
+
+# 사용 장치 선택(데스크탑 고정)
+find_visible("#estimateMainSearchOption > div > div.search_option_list > div:nth-child(2) > div.search_cate_contents > ul > li:nth-child(1) > label > span").click()
+
+# 제품 분류 선택
+options = finds_visible(
+    f"#estimateMainSearchOption > div > div.search_option_list > div:nth-child(3) > div.search_cate_contents > ul > li")
+    # f"#estimateMainSearchOption > div > div.search_option_list > div:nth-child(3) > div.search_cate_contents.open > ul > li")
+i = choose_one_op("메모리 제품 분류", options)
+find_visible(
+    f"#estimateMainSearchOption > div > div.search_option_list > div:nth-child(3) > div.search_cate_contents > ul > li:nth-child({i}) > label > span"
+    # f"#estimateMainSearchOption > div > div.search_option_list > div:nth-child(3) > div.search_cate_contents.open > ul > li:nth-child({i}) > label > span"    # 전체 보기 버튼 추가시 활용
+).click()
+
+time.sleep(2)
+
+memory_list = parse_products()
+
+for memory in memory_list:
+    print(memory)
+
+
+
+
+
 
 time.sleep(5)
 
