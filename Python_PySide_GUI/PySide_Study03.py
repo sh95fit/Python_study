0a5235a0be812afbcda87a78e2065c1e12565911
sh95fit <h95fit@gmail.com>
2022-12-26 11:25:34 +0900

    Qt Widgets - Layout(Horizontal, Vertical, Grid, Form)

---
+++ Python_PySide_GUI/PySide_Study03.py
@@ -0,0 +1,11 @@
+# PySide Widgets 활용
+'''
+Layout
+다른 위젯들의 구도를 잡아주는 역할
+Horizontal layout : 가로 배치
+Vertical layout : 세로 배치
+Grid layout : 격자로 나눠 widget들이 차지하는 범위를 잡아 격자 내 위치를 구성하는 것
+Form layout : 양식이 정해져 있음(요수에 맞춰 폼이 맞춰지는 것)
+
+
+'''
