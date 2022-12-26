a3944d80f8e3340fec69b24d64c07c38619c0a2b
sh95fit <h95fit@gmail.com>
2022-12-26 17:06:21 +0900

    PySide 실습(메신저 프로그램) - list view 활용

---
+++ Python_PySide_GUI/PySide_Study05.py
@@ -0,0 +1,54 @@
+# PySide 활용 - 메신저 프로그램
+'''
+- MVC 기법 == MVVM
+ Model, View, Controller
+ Model이 변형되면 알아서 아이템을 가져온다
+ ex> appendRow를 적용하면 줄을 추가하여 모델을 변형하므로 아이템을 가져와 리스트에 적용 시키는 것
+ 즉, 무엇이든 적용할 때 모델만 수정을 하면 알아서 반영이 된다!
+
+- list view vs list widget
+ list view : 모델이 없음. 직접 만들어줘야 한다
+ list widget : 모델을 가지고 있음
+'''
+
+import sys
+
+from PySide6.QtWidgets import QMainWindow, QApplication, QMessageBox, QTextBrowser
+
+from Qt_Messenger import Ui_MainWindow
+
+# list Widget이 아닌 list view를 사용하는 경우 모델을 선정해주어야 한다!
+from PySide6.QtGui import QStandardItemModel, QStandardItem
+
+class MainWindow(QMainWindow) :
+    def __init__(self) :
+        super(MainWindow, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        # list view의 모델을 만들어주기 위함!
+        self.model = QStandardItemModel()
+        self.ui.list_chat.setModel(self.model)
+
+        # 전송 버튼 처리
+        self.ui.btn_send.clicked.connect(self.send)
+    
+    def send(self) :
+        # list view를 사용하는 경우!
+        text = self.ui.edit_text.toPlainText()
+        item = QStandardItem(text)      # 모델은 StandardItemModel이므로  text가 아닌 StandardItem을 넣어줘야 한다!
+        self.model.appendRow(item)      # 줄을 추가한 후 위 텍스트를 반영
+
+
+        # 입력된 값이 전송 버튼에 의해 적용되는지 확인하는 용도!
+        # mb = QMessageBox()
+        # mb.setText(self.ui.edit_text.toPlainText())   
+        # mb.exec()
+
+if __name__ == "__main__" :
+    app = QApplication()
+
+    window = MainWindow()
+    window.show()
+
+    sys.exit(app.exec())
---
+++ Python_PySide_GUI/Qt_Messenger.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'Qt_Messenger.ui'
+##
+## Created by: Qt User Interface Compiler version 6.4.1
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QLabel, QLineEdit, QListView,
+    QMainWindow, QPushButton, QSizePolicy, QTextEdit,
+    QWidget)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(382, 601)
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.list_chat = QListView(self.centralwidget)
+        self.list_chat.setObjectName(u"list_chat")
+        self.list_chat.setGeometry(QRect(10, 10, 361, 411))
+        self.label = QLabel(self.centralwidget)
+        self.label.setObjectName(u"label")
+        self.label.setGeometry(QRect(30, 430, 81, 16))
+        font = QFont()
+        font.setPointSize(10)
+        font.setBold(True)
+        self.label.setFont(font)
+        self.label.setAlignment(Qt.AlignCenter)
+        self.label_2 = QLabel(self.centralwidget)
+        self.label_2.setObjectName(u"label_2")
+        self.label_2.setGeometry(QRect(140, 430, 241, 16))
+        self.label_2.setFont(font)
+        self.label_2.setAlignment(Qt.AlignCenter)
+        self.edit_nickname = QLineEdit(self.centralwidget)
+        self.edit_nickname.setObjectName(u"edit_nickname")
+        self.edit_nickname.setGeometry(QRect(10, 450, 121, 22))
+        self.edit_text = QTextEdit(self.centralwidget)
+        self.edit_text.setObjectName(u"edit_text")
+        self.edit_text.setGeometry(QRect(150, 450, 221, 111))
+        self.btn_send = QPushButton(self.centralwidget)
+        self.btn_send.setObjectName(u"btn_send")
+        self.btn_send.setGeometry(QRect(10, 570, 361, 24))
+        self.btn_send.setFont(font)
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"Hun's_Messenger", None))
+        self.label.setText(QCoreApplication.translate("MainWindow", u"\ub2c9\ub124\uc784", None))
+        self.label_2.setText(QCoreApplication.translate("MainWindow", u"\ucc44\ud305 \ub0b4\uc6a9", None))
+        self.btn_send.setText(QCoreApplication.translate("MainWindow", u"\uc804\uc1a1", None))
+    # retranslateUi
+
