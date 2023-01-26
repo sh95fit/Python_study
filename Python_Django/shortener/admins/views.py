c4f326b9f941e6598bde8d89246cf972e5b1c25d
sh95fit <h95fit@gmail.com>
2023-01-26 16:19:56 +0900

    Django queryset - .select_related, .prefetch_related

--- Python_Django/shortener/admins/views.py
+++ Python_Django/shortener/admins/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from shortener.urls.decorators import admin_only
 
 from django.db.models.query import Prefetch
-from shortener.models import ShortenedUrls
+from shortener.models import ShortenedUrls, Statistic
 
 
 @login_required
@@ -14,14 +14,35 @@ def url_list(request):
 
     urls = (
         ShortenedUrls.objects.order_by("-id")
+        # select_related
+        # 암시적으로 명시하지 않고 사용이 가능!
+        # But, 암시적으로 명시하지 않고 사용하는 경우 쿼리수가 증가하여 부하가 걸린다(즉, 명시하는 경우 쿼리수를 줄일 수 있다!)
+        # 데이터가 많아질수록 더 많은 쿼리를 수행하게 되어 부하가 크게 증가한다!
+        # 하나의 단일 테이블로 조인해서 여러 테이블에 분포된 데이터를 가져오는 경우 적합
+        # .select_related(
+        #     "creator",
+        #     "creator__user",
+        #     "creator__organization",
+        #     "creator__organization__pay_plan",
+        # )
+
+
+        # 데이터가 많고 조인을 많이 해야하는 경우 사용
         .prefetch_related(
             Prefetch("creator"),
             Prefetch("creator__user"),
             Prefetch("creator__organization"),
             Prefetch("creator__organization__pay_plan"),
             Prefetch("statistic_set"),
+            #     Prefetch("statistic_set", queryset=Statistic.objects.filter(
+            #         web_browser="Chrome"), to_attr="chrome_usage"),
         )
         .all()
     )
     print(urls)
+    # for u in urls:
+    #     print(u.chrome_usage)
+    #     for e in u.chrome_usage:
+    #         print(e.web_browser)
+
     return render(request, "admin_url_list.html", {"urls": urls})
